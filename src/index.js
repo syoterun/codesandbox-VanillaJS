7fe74c10482cde11ebe146cda2d541d6650af42b
syoterun <syoterun@gmail.com>
2022-12-11 22:36:06 +0900

    Spread syntax

--- src/index.js
+++ src/index.js
@@ -108,3 +108,48 @@
 // const [name, age] = myProfile;
 // const message4 = `名前は${name}です。年齢は${age}歳です。`;
 // console.log(message4);
+
+/**
+ * デフォルト値，引数
+ */
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん！`);
+// sayHello();
+
+/**
+ * スプレッド構文 ...
+ */
+// // 配列の展開
+// const arr1 = [1, 2];
+//console.log(arr1); // [1, 2]
+//console.log(...arr1); // 1 2
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]); // 3
+// sumFunc(...arr1); // 3
+
+// // まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1); // 1
+// console.log(num2); // 2
+// console.log(arr3); // [3, 4, 5]
+
+// // 配列のコピー,結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// // コピー
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr6); // [100, 20]
+// console.log(arr4); // [10, 20]
+
+// // 結合
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7); // [10, 20, 30, 40]
+
+// // =で配列をコピーすると参照元が同じになるため不具合のもととなる
+// const arr8 = arr4;
+// arr8[0] = 100;
+// console.log(arr8); // [100, 20]
+// console.log(arr4); // [100, 20]
